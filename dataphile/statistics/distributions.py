e6da5f7bf07152eb1b161be492c68db12eb1079b
Geoffrey Lentner <glentner@purdue.edu>
2018-06-14 14:45:04 -0400

    bump version to 0.1.0

--- dataphile/statistics/distributions.py
+++ dataphile/statistics/distributions.py
@@ -15,7 +15,7 @@
 """Statistical functions (e.g., polynomials, gaussians, etc.)
    dataphile.statistics.distributions
 
-   DataPhile, 0.0.2
+   DataPhile, 0.1.0
    Copyright (c) Geoffrey Lentner 2018. All rights reserved.
    GNU General Public License v3. See LICENSE file.
 """
