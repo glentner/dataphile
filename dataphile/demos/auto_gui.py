e9349bce5dbee6a8c2c42cab286ad5de4836d8fd
Geoffrey Lentner <glentner@purdue.edu>
2018-06-21 09:33:46 -0400

    bump version

--- dataphile/demos/auto_gui.py
+++ dataphile/demos/auto_gui.py
@@ -15,7 +15,7 @@
 """Demonstration of `dataphile.statistics.regression.modeling.AutoGUI`.
    dataphile.demos.auto_gui
 
-   DataPhile, 0.1.0
+   DataPhile, 0.1.1
    Copyright (c) Geoffrey Lentner 2018. All rights reserved.
    GNU General Public License v3. See LICENSE file.
 """
