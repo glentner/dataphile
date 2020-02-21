a28c3a8a21011079e954ebdc180b11a55bf55b8c
Geoffrey Lentner <glentner@purdue.edu>
2020-02-20 19:53:13 -0500

    version 0.2.0

--- dataphile/__meta__.py
+++ dataphile/__meta__.py
@@ -14,7 +14,7 @@
 
 
 __appname__   = 'dataphile'
-__version__   = '0.1.8'
+__version__   = '0.2.0'
 __authors__   = 'Geoffrey Lentner'
 __contact__   = '<glentner@gmail.com>'
 __license__   = 'Apache License'
